d146a58341aacd51bbda455e8bddddad937e0f3b
Vlad Osztrovszky <vlosztrovszky@gmail.com>
2024-11-13 22:33:01 +0200

    header luat de la rosca

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,14 @@
 import './Header.css'
 import logoGoEat from './GoeatLogo.png';
 import profile from './profile.png';
+import { useState } from 'react';
 
 function Header()
 {
+    const [dropdownVisible, setDropdownVisible] = useState(false);
+    const toggleDropdown = () => {
+        setDropdownVisible(!dropdownVisible);
+    }
     return(
         <header className="header">
             <img src={logoGoEat} width='100px' height='100px'/>
@@ -12,12 +17,23 @@ function Header()
                     <li><a href='#'>Inregistreaza-ti Restaurantul</a></li>
                     <li><a href='#'>Catalog</a></li>
                 </ul>
-                <div className="profile-dropdown">
-                    <img src={profile} width='50px' height='50px'></img>
-                    <span className="dropdown-arrow">▼</span>
+                <div className="profile-dropdown" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
+                    <div className="profile">
+                        <img src={profile} width='50px' height='50px'></img>
+                        <span className="dropdown-arrow">▼</span>
+                    </div>
+                {dropdownVisible && (
+                    <div className="profile-dropdown2">
+                        <ul>
+                            <li><a href='#'>Creaza Cont</a></li>
+                            <li><a href='#'>Autentificare</a></li>
+                        </ul>
+                    </div>
+                )}
                 </div>
+
             </div>
         </header>
     )
 }
-export default Header; 
+export default Header;
