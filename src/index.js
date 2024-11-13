313ca4091362d39b1ccec3801e2a604a6873dbab
Rosca Andrei <andrei_rosca03@yahoo.com>
2024-11-13 18:10:48 +0200

    mesaj

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import resPag from './resPag';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MainPage>
+    <MainPage/>
   </React.StrictMode>
 );
 
