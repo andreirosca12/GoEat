0f95c8826b9202f6ef3d88d4b307cc7152051ffe
Rosca Andrei <andrei_rosca03@yahoo.com>
2024-11-13 16:12:02 +0200

    login facut

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css';
 import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 import App from './App';
+import Login from './Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header/>
-    <App />
+    <Login/>
   </React.StrictMode>
 );
 
