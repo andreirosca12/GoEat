d422aebd822fb0764175d5cd7fb790cefd43208f
Vlad Osztrovszky <vlosztrovszky@gmail.com>
2024-11-13 20:41:57 +0200

    index.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import Header from './Header';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
-import resPag from './resPag';
-import MainPage from './mainPage';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <MainPage/>
-  </React.StrictMode>
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
